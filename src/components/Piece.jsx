a6066e8de59b8fa0b5018dc23c5473e1601d01d4
Jromeo15 <jromeeo_15@hotmail.com>
2026-05-21 11:51:17 +0200

    drag working version

--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -6,6 +6,9 @@ export default function Piece() {
 
   const offsetRef = useRef({ x: 0, y: 0 });
 
+  // -------------------------
+  // MOUSE
+  // -------------------------
   const onMouseDown = (e) => {
     draggingRef.current = true;
 
@@ -28,19 +31,57 @@ export default function Piece() {
     draggingRef.current = false;
   };
 
+  // -------------------------
+  // TOUCH (DEDOS)
+  // -------------------------
+  const onTouchStart = (e) => {
+    const t = e.touches[0];
+
+    draggingRef.current = true;
+
+    offsetRef.current = {
+      x: t.clientX - pos.x,
+      y: t.clientY - pos.y,
+    };
+  };
+
+  const onTouchMove = (e) => {
+    if (!draggingRef.current) return;
+
+    const t = e.touches[0];
+
+    setPos({
+      x: t.clientX - offsetRef.current.x,
+      y: t.clientY - offsetRef.current.y,
+    });
+  };
+
+  const onTouchEnd = () => {
+    draggingRef.current = false;
+  };
+
   useEffect(() => {
+    // mouse
     window.addEventListener("mousemove", onMouseMove);
     window.addEventListener("mouseup", onMouseUp);
+
+    // touch
+    window.addEventListener("touchmove", onTouchMove);
+    window.addEventListener("touchend", onTouchEnd);
 
     return () => {
       window.removeEventListener("mousemove", onMouseMove);
       window.removeEventListener("mouseup", onMouseUp);
+
+      window.removeEventListener("touchmove", onTouchMove);
+      window.removeEventListener("touchend", onTouchEnd);
     };
   }, []);
 
   return (
     <div
       onMouseDown={onMouseDown}
+      onTouchStart={onTouchStart}
       style={{
         width: 50,
         height: 50,
@@ -50,6 +91,7 @@ export default function Piece() {
         top: pos.y,
         cursor: "grab",
         userSelect: "none",
+        touchAction: "none",
       }}
     />
   );
