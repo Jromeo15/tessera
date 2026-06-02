c909a082496057f511ce236f0b4fa277ff107801
Jromeo15 <jromeeo_15@hotmail.com>
2026-06-02 12:40:16 +0200

    Avance

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,10 +1,27 @@
 import { useAuth } from "../context/AuthContext";
+import { login, logout } from "../lib/auth";
 
 export default function UserMenu() {
   const { user } = useAuth();
 
+  async function handleAuth() {
+    if (user) {
+      await logout();
+    } else {
+      const email = prompt("Email:");
+      const password = prompt("Password:");
+
+      const { error } = await login(email, password);
+
+      if (error) {
+        alert("Error login");
+        console.log(error);
+      }
+    }
+  }
+
   return (
-    <div className="userMenu">
+    <div className="userMenu" onClick={handleAuth}>
       {user ? user.email : "Iniciar sesión"}
     </div>
   );
