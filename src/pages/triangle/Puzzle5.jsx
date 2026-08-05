c5517126f58cdc29ea1c52328b4c957a02d9cb9c
Jromeo15 <jromeeo_15@hotmail.com>
2026-08-05 10:24:48 +0200

    Avance

---
+++ src/pages/triangle/Puzzle5.jsx
@@ -0,0 +1,14 @@
+import PuzzleLayout from "../../layout/PuzzleLayout";
+import { SHAPES } from "./shapes/shapes5";
+
+export default function App({ onBack }) {
+  return (
+    <PuzzleLayout
+      title="Puzzle 5"
+      category="triangle"
+      puzzleIndex={5}
+      shapes={SHAPES}
+      onBack={onBack}
+    />
+  );
+}
