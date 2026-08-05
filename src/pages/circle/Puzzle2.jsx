2a84fb2d313d645e7c80fc36bdfff911b4dc166c
Jromeo15 <jromeeo_15@hotmail.com>
2026-08-05 19:08:11 +0200

    Avance

---
+++ src/pages/circle/Puzzle2.jsx
@@ -0,0 +1,14 @@
+import PuzzleLayout from "../../layout/PuzzleLayout";
+import { SHAPES } from "./shapes/shapes2";
+
+export default function App({ onBack }) {
+  return (
+    <PuzzleLayout
+      title="Puzzle 2"
+      category="circle"
+      puzzleIndex={2}
+      shapes={SHAPES}
+      onBack={onBack}
+    />
+  );
+}
